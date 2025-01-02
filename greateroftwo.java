a2340830ed497e91bc7125a7c9f2b8bb8fb7d57f
Avin Vardhan_07 <vardhan99590@gmail.com>
2025-01-02 14:19:36 +0530

    second commit

---
+++ greateroftwo.java
@@ -0,0 +1,19 @@
+public class greateroftwo {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 20;
+
+
+        if (num1 > num2){
+            System.out.println("greater is: num1");
+        }
+
+        else if (num1 == num2){
+          System.out.println("equal");
+        }
+        
+        else{
+            System.out.println("num2");
+        }
+    }
+}
