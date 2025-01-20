609ff88f7d69912e92555e76507f234ce0e0d7fb
Avin Vardhan_07 <vardhan99590@gmail.com>
2025-01-20 12:14:33 +0530

    java program based on ArrayList using the collection framework

---
+++ Demo029.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Demo029 {
+    public static void main(String[] args) {
+          ArrayList<String> list = new ArrayList<>();
+
+
+          list.add("avin");
+          list.add("vardhan");
+          list.add("surya");
+          list.add("bharath");
+          list.add("bob");
+          list.add("cristopher");
+          list.add("jk");
+          list.add("danny");
+          list.add("swaroop");
+          list.add("john");
+         
+          System.err.println(list);
+    }
+}
