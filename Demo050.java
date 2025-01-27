21afabcff9774308f277a411b32bfa5a2ec54f91
Avin Vardhan_07 <vardhan99590@gmail.com>
2025-01-27 11:13:53 +0530

    printing the multiplication of number 5

---
+++ Demo050.java
@@ -0,0 +1,10 @@
+public class Demo050 {
+    public static void main(String[] args) {
+        int end = 10;
+        int num = 5;
+
+        for (int i = 1; i <= end ; i++){
+            System.out.println(num + " *" + i +" =" + (num*i));
+        }
+    }
+}
