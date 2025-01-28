a9efc1c96f6f0b0fbc4b5ca61d331f89080a50b6
Avin Vardhan_07 <vardhan99590@gmail.com>
2025-01-28 22:20:58 +0530

    collcetion framework in arraylist

--- Demo026.java
+++ Demo026.java
@@ -23,6 +23,7 @@ class Count{
 class Demo026{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        sc.close();
         System.out.println("Enter the string: ");
         String str = sc.nextLine();
         Count c = new Count();
