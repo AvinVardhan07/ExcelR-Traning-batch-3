0aec3a03771f3514978801610389eb3814bd26bc
Avin Vardhan_07 <vardhan99590@gmail.com>
2025-01-28 22:13:09 +0530

    calculating the remaining days in a year according to the user input

--- Demo018.java
+++ Demo018.java
@@ -9,5 +9,6 @@ class Demo018{
         long years = minutes / (60 * 24 * 365);
         long remainingDays = (minutes % (60 * 24 * 365)) / (60 * 24);
         System.out.println("Years: "+years+" Days: "+remainingDays);
+        sc.close();
     }
 }
